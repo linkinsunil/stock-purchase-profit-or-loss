caf14021c9447a5ea899ea55d86acab678bc8e21
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-08-17 01:40:43 +0530

    wip

---
+++ index.js
@@ -0,0 +1,7 @@
+const initialPrice = document.querySelector(".initial-price");
+const stocksQuantity = document.querySelector(".stocks-quantity");
+const currentPrice = document.querySelector(".currentPrice")
+const btnCheckStocksStatus = document.querySelector("#btn-checkStocksStatus")
+const message = document.querySelector("#message")
+
+console.log(btnCheckStocksStatus)
